56400aa378f20279134d18d75b8ebd5290822973
vmrathod <vishal.amitus@gmail.com>
2016-02-10 14:54:24 +0530

    added tests

--- app/identidock.py
+++ app/identidock.py
@@ -1,7 +1,8 @@
-from flask import Flask, Response,request
+from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
+import html
 
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
@@ -9,16 +10,14 @@ salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
 
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
 
     name = default_name
     if request.method == 'POST':
-        name = request.form['name']
-        
+        name = html.escape(request.form['name'], quote=True)
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
-    
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
               Hello <input type="text" name="name" value="{0}">
@@ -26,18 +25,23 @@ def mainpage():
               </form>
               <p>You look like a:
               <img src="/monster/{1}"/>
-              '''.format(name,name_hash)
+              '''.format(name, name_hash)
     footer = '</body></html>'
 
     return header + body + footer
+
+
 @app.route('/monster/<name>')
 def get_identicon(name):
-        image = cache.get(name)
-        if image is None:
-                print ("Cache miss", flush=True)
-                r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-                image = r.content
-                cache.set(name, image)
-        return Response(image, mimetype='image/png')    
+
+    image = cache.get(name)
+    if image is None:
+        print ("Cache miss", flush=True)
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+        image = r.content
+        cache.set(name, image)
+
+    return Response(image, mimetype='image/png')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
